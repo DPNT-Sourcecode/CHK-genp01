a4bf7283daf7ad6a43c2bd0442404b14f80aac95
sbx_user1051 <sbx_user1051@169.254.75.197>
2023-11-21 19:01:14 +0000

    Tue Nov 21 19:01:14 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -9,9 +9,29 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         System.out.println(skus);
         List<String> skuList = Arrays.stream(skus.split(",")).toList();
-        skuList.stream().mapToInt(e -> ).o;
+        return skuList.stream().mapToInt(p -> getProductPrice(skus)).sum();
+    }
+
+    private Integer getProductPrice(String sku){
+        switch (sku){
+            case "A":
+                return 50;
+            case "B":
+                return 30;
+            case "C":
+                return 20;
+            case "D":
+                return 15;
+            case "3A":
+                return 130;
+            case "2B":
+                return 45;
+            default:
+                return -1;
+        }
     }
 
 
 }
+
 
